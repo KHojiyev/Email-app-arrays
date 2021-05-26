022fe13467d9ff872a6d9f47833f0045057a5119
Lochinbek <80982678+KHojiyev@users.noreply.github.com>
2021-05-26 13:01:41 +0500

    Signed-off-by: Lochinbek <80982678+KHojiyev@users.noreply.github.com>

---
+++ Email.java
@@ -0,0 +1,76 @@
+package Module2_OOP.ExtraTasks.Lesson3.EmailAplication_arrays;
+
+public class Email {
+
+    private String subject;
+    private String massage;
+    private User sender;
+    private User receiver;
+    private boolean status;
+
+    public Email(String subject, String massage, User sender, User receiver, boolean status) {
+        this.subject = subject;
+        this.massage = massage;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.status = status;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+
+    public String checkStatus() {
+        if (status) {
+            return "Unread";
+        } else {
+            return "Read";
+        }
+    }
+
+    public String toString() {
+        return "Subject: " + getSubject() + " || " +
+                "Massage: " + getMassage() + " || " +
+                "Sender: " + getSender().getEmailAddress() + " || " +
+                "Receiver: " + getReceiver().getEmailAddress() + " || " +
+                "Status: " + checkStatus();
+    }
+
+}
